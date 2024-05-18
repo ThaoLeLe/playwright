8046e360aa1c4390204c5f9ca03e246b6e5c0977
Thao <75958858+ThaoLeLe@users.noreply.github.com>
2024-05-18 17:25:35 +0700

    update log info

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,5 +1,11 @@
 const { test, expect } = require("@playwright/test");
 const invoices = require("../resources/invoices.json");
+
+var information = {
+  page_success: 0,
+  waiting: 0,
+  error: 0
+};
 for (const invoice of invoices) {
   test(`Test with invoice ${invoice.invoice_identification}`, async function ({ page }) {
     await page.goto(invoice.url, { timeout: 100000 });
@@ -8,15 +14,21 @@ for (const invoice of invoices) {
       // Kiểm tra xem biểu mẫu với id="payForm" có hiển thị trên trang hay không
       const isPayFormVisible = await page.isVisible('//form[@id="payForm"]');
       if (isPayFormVisible) {
-        console.log('Biểu mẫu thanh toán đã hiển thị.');
+        console.log('Trang thanh toan');
+        information.page_success++;
       } else {
         // Nếu không tìm thấy biểu mẫu thanh toán, kiểm tra trang "PLEASE WAIT"
         const isPleaseWaitVisible = await page.isVisible('//h3[contains(text(),"PLEASE WAIT")]');
         if (isPleaseWaitVisible) {
-          console.log('Trang hiện hàng chờ của hệ thống đã được hiển thị.');
+          console.log('Trang hang doi');
+          const content = await page.textContent('p#index');
+          information.waiting++;
         } else {
-          console.error('Không tìm thấy trang thanh toán hay trang hàng chờ.');
+          console.error('Trang loi');
+          information.error++;
         }
+
+        console.log(`Information: ${JSON.stringify(information)}`);
       }
     } catch (e) {
       console.error('Đã xảy ra lỗi: ', e);
