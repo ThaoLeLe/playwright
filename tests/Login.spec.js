34455c0506c644c3b489b2b274ea68e35113e129
Thao <75958858+ThaoLeLe@users.noreply.github.com>
2024-05-18 16:08:53 +0700

    new verify

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -5,13 +5,31 @@ for (const invoice of invoices) {
     await page.goto(invoice.url, { timeout: 100000 });
     //wait 5 seconds
     try {
-      // Kiểm tra xem biểu mẫu với id="payForm" có tồn tại trên trang hay không
-      await page.waitForSelector('form#payForm', { timeout: 5000 });
-      console.log('Trang thanh toán đã được hiển thị.');
+      // Kiểm tra xem biểu mẫu với id="payForm" có hiển thị trên trang hay không
+      const isPayFormVisible = await page.isVisible('//form[@id="payForm"]');
+      if (isPayFormVisible) {
+        console.log('Biểu mẫu thanh toán đã hiển thị.');
+      } else {
+        // Nếu không tìm thấy biểu mẫu thanh toán, kiểm tra trang "PLEASE WAIT"
+        const isPleaseWaitVisible = await page.isVisible('//h3[contains(text(),"PLEASE WAIT")]');
+        if (isPleaseWaitVisible) {
+          console.log('Trang hiện hàng chờ của hệ thống đã được hiển thị.');
+        } else {
+          console.log('Không tìm thấy trang thanh toán hay trang hàng chờ.');
+        }
+      }
     } catch (e) {
-      console.log('Không tìm thấy biểu mẫu thanh toán.');
-      await page.waitForSelector('//h3[contains(text(),"PLEASE WAIT")]', { timeout: 5000 });
+      console.log('Đã xảy ra lỗi: ', e);
     }
+
+    // try {
+    //   // Kiểm tra xem biểu mẫu với id="payForm" có tồn tại trên trang hay không
+    //   await page.waitForSelector('form#payForm', { timeout: 5000 });
+    //   console.log('Trang thanh toán đã được hiển thị.');
+    // } catch (e) {
+    //   console.log('Không tìm thấy biểu mẫu thanh toán.');
+    //   await page.waitForSelector('//h3[contains(text(),"PLEASE WAIT")]', { timeout: 5000 });
+    // }
     
     //verify page load have //form[@id="payForm"]
 
