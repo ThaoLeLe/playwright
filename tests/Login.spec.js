b2c614308dd66dc62130669a1a543b318dd8e276
Thao <75958858+ThaoLeLe@users.noreply.github.com>
2024-05-18 15:12:39 +0700

    verify testcase

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -4,6 +4,26 @@ for (const invoice of invoices) {
   test(`Test with invoice ${invoice.invoice_identification}`, async function ({ page }) {
     await page.goto(invoice.url, { timeout: 100000 });
     //wait 5 seconds
+    try {
+      // Kiểm tra xem biểu mẫu với id="payForm" có tồn tại trên trang hay không
+      await page.waitForSelector('form#payForm', { timeout: 5000 });
+      console.log('Trang thanh toán đã được hiển thị.');
+    } catch (e) {
+      console.log('Không tìm thấy biểu mẫu thanh toán.');
+      await page.waitForSelector('//h3[contains(text(),"PLEASE WAIT")]', { timeout: 5000 });
+    }
+    
+    //verify page load have //form[@id="payForm"]
+
+
+   
+
+
+
+
+
+
+
 
   });
 }
