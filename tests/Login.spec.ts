283185ccaf53e539e285041b1706932387cf46ca
Phu Hoang <mchoang98@gmail.com>
2024-05-20 14:55:05 +0700

    update total result

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -8,52 +8,61 @@ const aggregatedInformation = {
   error: 0,
 };
 
-// Iterate over invoices and create tests
-for (const invoice of invoices) {
-  test(`Test with invoice ${invoice.invoice_identification}`, async ({ page }) => {
-    await page.goto(invoice.url, { timeout: 100000 });
+test.beforeAll(() => {
+  // Iterate over invoices and create tests
+  for (const invoice of invoices) {
+    test(`Test with invoice ${invoice.invoice_identification}`, async ({
+      page,
+    }) => {
+      await page.goto(invoice.url, { timeout: 100000 });
 
-    try {
-      const information = {
-        page_success: 0,
-        waiting: 0,
-        error: 0,
-      };
+      try {
+        const information = {
+          page_success: 0,
+          waiting: 0,
+          error: 0,
+        };
 
-      const isPleaseWaitVisible = await page.isVisible('//h3[contains(text(),"PLEASE WAIT")]', { timeout: 100000 });
-      if (isPleaseWaitVisible) {
-        console.log("Trang hang doi");
-        information.waiting++;
-      } else {
-        const iframe = await page.frameLocator("iframe");
-        if (iframe) {
-          const payForm = await iframe.locator("#payForm");
-          if (payForm) {
-            console.log("Trang thanh toan", payForm);
-            information.page_success++;
+        const isPleaseWaitVisible = await page.isVisible(
+          '//h3[contains(text(),"PLEASE WAIT")]',
+          { timeout: 100000 }
+        );
+        if (isPleaseWaitVisible) {
+          console.log("Trang hang doi");
+          information.waiting++;
+        } else {
+          const iframe = await page.frameLocator("iframe");
+          if (iframe) {
+            const payForm = await iframe.locator("#payForm");
+            if (payForm) {
+              console.log("Trang thanh toan", payForm);
+              information.page_success++;
+            } else {
+              console.error("Trang loi");
+              information.error++;
+            }
           } else {
             console.error("Trang loi");
             information.error++;
           }
-        } else {
-          console.error("Trang loi");
-          information.error++;
         }
+
+        // Aggregate the information
+        aggregatedInformation.page_success += information.page_success;
+        aggregatedInformation.waiting += information.waiting;
+        aggregatedInformation.error += information.error;
+
+        console.log(`Information: ${JSON.stringify(information)}`);
+      } catch (e) {
+        console.error("Đã xảy ra lỗi: ", e);
       }
-
-      // Aggregate the information
-      aggregatedInformation.page_success += information.page_success;
-      aggregatedInformation.waiting += information.waiting;
-      aggregatedInformation.error += information.error;
-
-      console.log(`Information: ${JSON.stringify(information)}`);
-    } catch (e) {
-      console.error("Đã xảy ra lỗi: ", e);
-    }
-  });
-}
+    });
+  }
+});
 
 // After all tests have run, output the aggregated information
 test.afterAll(() => {
-  console.log(`Aggregated Information: ${JSON.stringify(aggregatedInformation)}`);
+  console.log(
+    `Aggregated Information: ${JSON.stringify(aggregatedInformation)}`
+  );
 });
