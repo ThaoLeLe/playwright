1740cf0cf4dab3f5a984b2c7f891e8f89f05a061
Phu Hoang <mchoang98@gmail.com>
2024-05-20 16:02:44 +0700

    update flow

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 // INVOICE_BATCH_FILE is environment variable
-const invoices = require(`./resources/invoices/${process.env.INVOICE_BATCH_FILE}`);
+const invoices = require(`../resources/invoices/${process.env.INVOICE_BATCH_FILE}`);
 // Shared variable to aggregate results
 const aggregatedInformation = {
   page_success: 0,
