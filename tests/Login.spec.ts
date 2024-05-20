f6064c31b00054ecf719f9d726b030dde36576f6
Phu Hoang <mchoang98@gmail.com>
2024-05-20 16:58:23 +0700

    update playwright

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,12 +1,31 @@
 const { test, expect } = require("@playwright/test");
+const axios = require('axios');
 // INVOICE_BATCH_FILE is environment variable
 const invoices = require(`../resources/invoices/${process.env.INVOICE_BATCH_FILE}`);
 // Shared variable to aggregate results
-const aggregatedInformation = {
-  page_success: 0,
-  waiting: 0,
-  error: 0,
-};
+
+async function sendSlackMessage( message: string) {
+  const url = "https://slack.com/api/chat.postMessage";
+  const headers = {
+      "Authorization": "Bearer xoxb-6519771859383-7087657215015-iWDfftKGa81LAgIzlPBGV9WA",
+      "Content-Type": "application/json"
+  };
+  const data = {
+      "channel": '#playwright',
+      "text": message
+  };
+
+  try {
+      const response = await axios.post(url, data, { headers });
+      if (response.status === 200) {
+          console.log("Message sent successfully!");
+      } else {
+          console.error("Failed to send message. Error:", response.data);
+      }
+  } catch (error) {
+      console.error("Failed to send message. Error:", error);
+  }
+}
 
 // Iterate over invoices and create tests
 for (const invoice of invoices) {
@@ -30,6 +49,8 @@ for (const invoice of invoices) {
           const payForm = await iframe.locator("#payForm");
           if (payForm) {
             console.log("Trang thanh toan", payForm);
+            await sendSlackMessage( `Trang thanh toan: ${invoice.url} success`);
+
             information.page_success++;
           } else {
             console.error("Trang loi");
@@ -40,12 +61,6 @@ for (const invoice of invoices) {
           information.error++;
         }
       }
-
-      // Aggregate the information
-      aggregatedInformation.page_success += information.page_success;
-      aggregatedInformation.waiting += information.waiting;
-      aggregatedInformation.error += information.error;
-
       console.log(`Information: ${JSON.stringify(information)}`);
     } catch (e) {
       console.error("Đã xảy ra lỗi: ", e);
