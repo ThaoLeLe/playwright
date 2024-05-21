d7b4434e32bda479e59cbe0b55d0a074d7f6e34b
Phu Hoang <mchoang98@gmail.com>
2024-05-21 14:41:01 +0700

    remove axios

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -2,7 +2,6 @@ const { test, expect } = require("@playwright/test");
 // INVOICE_BATCH_FILE is environment variable
 const invoices = require(`../resources/invoices/${process.env.INVOICE_BATCH_FILE}`);
 // Shared variable to aggregate results
-const axios = require("axios");
 
 // Iterate over invoices and create tests
 for (const invoice of invoices) {
