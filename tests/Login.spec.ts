0fb000a54bf70daf409d6bb7eb33ed6e69f6a383
Phu Hoang <mchoang98@gmail.com>
2024-05-21 16:10:57 +0700

    remove check not works playwright

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 // INVOICE_BATCH_FILE is environment variable
-const invoices = require(`../resources/invoices`);
+const invoices = require(`../resources/invoices/${process.env.INVOICE_BATCH_FILE}`);
 // Shared variable to aggregate results
 
 // Iterate over invoices and create tests
@@ -10,38 +10,5 @@ for (const invoice of invoices) {
   }) => {
     await page.goto(invoice.url, { timeout: 100000 });
 
-    try {
-      const information = {
-        page_success: 0,
-        waiting: 0,
-        error: 0,
-      };
-
-      await page.waitForTimeout(3000);
-
-      const isPleaseWaitVisible = await page.isVisible(
-        '//h3[contains(text(),"PLEASE WAIT")]',
-        { timeout: 100000 }
-      );
-      if (isPleaseWaitVisible) {
-        console.log("Trang hang doi");
-        information.waiting++;
-      } else {
-        const iframe = await page.frameLocator("iframe");
-        if (iframe) {
-          const payForm = await iframe.locator("#payForm");
-          if (payForm) {
-            console.log("Trang thanh toan", payForm);
-            information.page_success++;
-          } else {
-            console.error("Trang loi");
-            information.error++;
-          }
-        }
-      }
-      console.log(`Information: ${JSON.stringify(information)}`);
-    } catch (e) {
-      console.error("Đã xảy ra lỗi: ", e);
-    }
   });
 }
