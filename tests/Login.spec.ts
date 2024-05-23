37287d8f5e31e8101017a4fdf54f602c71adfb08
Phu Hoang <mchoang98@gmail.com>
2024-05-23 09:59:51 +0700

    update url

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -8,7 +8,7 @@ for (const invoice of invoices) {
   test(`Test with invoice ${invoice.invoice_identification}`, async ({
     page,
   }) => {
-    await page.goto("https://ezactive.com", { timeout: 100000 });
+    await page.goto("https://www.ezactive.com/hkfa_websocket", { timeout: 100000 });
 
     await expect(page).toHaveURL(invoice.url);
 
