fb2e92a9ac08a0b0b52b4d48b846378af4bfbc65
Phu Hoang <mchoang98@gmail.com>
2024-05-24 11:38:10 +0700

    update log

--- generate-matrix.js
+++ generate-matrix.js
@@ -5,4 +5,7 @@ const fs = require('fs');
 const totalBatches = process.argv[2] || 150;
 const matrix = Array.from({ length: totalBatches }, (_, i) => i + 1);
 
+
 fs.writeFileSync('matrix.json', JSON.stringify(matrix));
+
+console.log(`Created ${matrix.length} matrix files.`);
